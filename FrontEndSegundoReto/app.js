5bf2f543f59a3a76c30c09369d13062f4f99d126
cuauhtlahuac <cuitlagua@gmail.com>
2018-02-06 04:30:16 +0000

    SegundoReto

---
+++ FrontEndSegundoReto/app.js
@@ -0,0 +1,12 @@
+//Segundo Reto
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+export default class SegundoReto extends Component {
+  render() {
+    return (
+      <View style={{marginTop: '70%',marginRight: '20%',width: '30%',height: '100',backgroundColor: 'black', alignSelf: 'flex-end'}}>
+      </View>
+    );
+  }
+}
